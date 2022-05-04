d4299f129d8c379527bdba5c8d3d6a3156a1cb4c
Samoe97 <56936331+Samoe97@users.noreply.github.com>
2022-05-03 17:50:24 -0700

    ...

--- toolbox/keyringDPG.py
+++
@@ -1,9 +0,0 @@
-# Keys
-
-class Keyring():
-    key = 0
-    secret = 0
-
-HPSiteFlow = Keyring()
-HPSiteFlow.key = "2534280133710"
-HPSiteFlow.secret = "601232626eed3f3dcdaec1d47dca57b69a0b6bffb281bb60"
